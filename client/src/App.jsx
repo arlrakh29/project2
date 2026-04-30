921c1ad9c37016f662d19261dfff07bcf89f102d
Arlan <arlrakh29@nuevaschool.org>
2026-04-30 10:16:19 -0700

    input button added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,55 @@
 import { useState } from 'react'
 import './App.css'
 
+const [planeInfo, setPlaneInfo] = useState({
+  planetype: "",
+  location: "",
+  date: "",
+});
+
+function Input() {
+  return (
+    <div>
+      <form>
+        <div>
+          <input
+            type="text"
+            name="planetype"
+            placeholder="Plane type"
+            value={planeInfo.planetype}
+          />
+        </div>
+        <div>
+          <input
+            type="location"
+            name="location"
+            placeholder="Location"
+            value={planeInfo.location}
+          />
+        </div>
+        <div>
+          <input
+            type="date"
+            name="date"
+            placeholder="Date spotted"
+            value={planeInfo.date}
+          />
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
 function App() {
   return (
     <>
-      <h1>AAIRPLANE SPOTTING TRACKER</h1>
+      <h1>AIRPLANE SPOTTING TRACKER</h1>
+      <Input />
     </>
   )
 }
 
-export default App
+export default App;
